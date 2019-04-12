79b4de0f2b7f98fb13ad7a955a4e7f2007acfc62
Anton Golub <mailbox@antongolub.ru>
2019-04-12 21:53:20 +0300

    test(protopipe): add test for lambda api

--- src/test/ts/protopipe.ts
+++ src/test/ts/protopipe.ts
@@ -1,5 +1,10 @@
 import {
-  IArrow, IExecutor, IExecutorContext, IExecutorOutput, IGraph, IHandler,
+  IArrow,
+  IExecutor,
+  IExecutorContext,
+  IExecutorOutput,
+  IGraph,
+  IHandler,
   IInput,
   IOutput, ITraverser,
   ITraverserInput,
@@ -7,7 +12,7 @@ import {
 } from '../../main/ts/interface'
 
 import Graph from '../../main/ts/graph'
-import {Protopipe} from '../../main/ts/protopipe'
+import {Protopipe, protopipe} from '../../main/ts/protopipe'
 import executor from '../../main/ts/executor'
 
 describe('Protopipe', () => {
@@ -35,24 +40,180 @@ describe('Protopipe', () => {
     return {meta: {...meta, sequence: [...meta.sequence, next.tail]}}
   }
 
-  const protopipe = new Protopipe({
-    graph,
-    traverser,
-    handler,
-  })
+  describe('class', () => {
+    const protopipe = new Protopipe({
+      graph,
+      traverser,
+      handler,
+    })
 
-  describe('constructor', () => {
-    it('returns proper instance', () => {
-      expect(protopipe).toBeInstanceOf(Protopipe)
-      expect(protopipe.handler).toBe(handler)
-      expect(protopipe.traverser).toBe(traverser)
-      expect(protopipe.executor).toBe(executor)
+    describe('constructor', () => {
+      it('returns proper instance', () => {
+        expect(protopipe).toBeInstanceOf(Protopipe)
+        expect(protopipe.handler).toBe(handler)
+        expect(protopipe.traverser).toBe(traverser)
+        expect(protopipe.executor).toBe(executor)
+      })
+    })
+
+    describe('proto', () => {
+      it('#process processes data', () => {
+        const res = protopipe.process(input)
+
+        expect(res).toEqual({
+          opts: {},
+          data: {
+            count: 2,
+          },
+          meta: {
+            sequence: [
+              'A',
+              'B',
+              'C',
+            ],
+          },
+        })
+      })
+    })
+
+    describe('static', () => {
+      describe('#handler', () => {
+        it('works as identity', () => {
+          const input = {
+            data: {},
+            meta: {sequence: []},
+            opts: {},
+          }
+          expect(Protopipe.handler(input)).toBe(input)
+        })
+      })
+
+      describe('#traverser', () => {
+        const _input = {
+          data: {},
+          graph: {
+            vertexes: ['A', 'B', 'C'],
+            arrows: [
+              {head: 'A', tail: 'B'},
+              {head: 'B', tail: 'C'},
+            ],
+          },
+          meta: {
+            sequence: [],
+          },
+          opts: {},
+        }
+
+        it('resolves the first graph vertex as entry point (empty sequence)', () => {
+          const input: ITraverserInput = {
+            ..._input,
+            meta: {
+              sequence: [],
+            },
+          }
+
+          expect(Protopipe.traverser(input)).toEqual({
+            meta: {
+              sequence: ['A'],
+            },
+          })
+        })
+
+        it('resolves next vertex by prev meta.sequence and graph declaration', () => {
+          expect(Protopipe.traverser({
+            ..._input,
+            meta: {
+              sequence: ['B'],
+            },
+          })).toEqual({
+            meta: {
+              sequence: ['B', 'C'],
+            },
+          })
+        })
+
+        it('returns null if path is not found', () => {
+          expect(Protopipe.traverser({
+            ..._input,
+            meta: {
+              sequence: ['D'],
+            },
+          })).toEqual(null)
+        })
+      })
+
+      describe('#graph', () => {
+        it('contains default empty graph', () => {
+          expect(Protopipe.graph).toEqual({
+            vertexes: [],
+            arrows: [],
+          })
+        })
+      })
+
+      describe('#parser', () => {
+        it('extracts protopipe opt from params', () => {
+          const handler: IHandler = (input: IInput): IOutput => input
+          const graph: IGraph = new Graph({
+            vertexes: [],
+            arrows: [],
+          })
+          const traverser: ITraverser = (input: ITraverserInput): ITraverserOutput => input
+          const executor: IExecutor = (context: IExecutorContext): IExecutorOutput => context
+          const params = {
+            graph,
+            handler,
+            traverser,
+            executor,
+          }
+          const opts = Protopipe.parser(params)
+
+          expect(opts).toEqual(params)
+        })
+
+        it('injects default values otherwise', () => {
+          const opts = Protopipe.parser({})
+
+          expect(opts).toEqual({
+            graph: Protopipe.graph,
+            handler: Protopipe.handler,
+            traverser: Protopipe.traverser,
+            executor: Protopipe.executor,
+          })
+        })
+
+        it('supports parser impl override', () => {
+          const opts = Protopipe.parser({
+            parser() {
+              return {
+                graph: Protopipe.graph,
+                handler: Protopipe.handler,
+                traverser: Protopipe.traverser,
+                executor: Protopipe.executor,
+                foo: 'bar',
+              }
+            },
+          })
+
+          expect(opts).toEqual({
+            graph: Protopipe.graph,
+            handler: Protopipe.handler,
+            traverser: Protopipe.traverser,
+            executor: Protopipe.executor,
+            foo: 'bar',
+          })
+        })
+      })
     })
   })
 
-  describe('proto', () => {
-    it('#process processes data', () => {
-      const res = protopipe.process(input)
+  describe('lambda-function', () => {
+    it('processes data', () => {
+      const res = protopipe({
+        handler,
+        traverser,
+        graph
+      })(input)
 
       expect(res).toEqual({
         opts: {},
@@ -66,136 +227,6 @@ describe('Protopipe', () => {
             'C',
           ],
         },
-      })
-    })
-  })
-
-  describe('static', () => {
-    describe('#handler', () => {
-      it('works as identity', () => {
-        const input = {
-          data: {},
-          meta: {sequence: []},
-          opts: {},
-        }
-        expect(Protopipe.handler(input)).toBe(input)
-      })
-    })
-
-    describe('#traverser', () => {
-      const _input = {
-        data: {},
-        graph: {
-          vertexes: ['A', 'B', 'C'],
-          arrows: [
-            {head: 'A', tail: 'B'},
-            {head: 'B', tail: 'C'},
-          ],
-        },
-        meta: {
-          sequence: [],
-        },
-        opts: {},
-      }
-
-      it('resolves the first graph vertex as entry point (empty sequence)', () => {
-        const input: ITraverserInput = {
-          ..._input,
-          meta: {
-            sequence: [],
-          },
-        }
-
-        expect(Protopipe.traverser(input)).toEqual({
-          meta: {
-            sequence: ['A'],
-          },
-        })
-      })
-
-      it('resolves next vertex by prev meta.sequence and graph declaration', () => {
-        expect(Protopipe.traverser({
-          ..._input,
-          meta: {
-            sequence: ['B'],
-          },
-        })).toEqual({
-          meta: {
-            sequence: ['B', 'C'],
-          },
-        })
-      })
-
-      it('returns null if path is not found', () => {
-        expect(Protopipe.traverser({
-          ..._input,
-          meta: {
-            sequence: ['D'],
-          },
-        })).toEqual(null)
-      })
-    })
-
-    describe('#graph', () => {
-      it('contains default empty graph', () => {
-        expect(Protopipe.graph).toEqual({
-          vertexes: [],
-          arrows: [],
-        })
-      })
-    })
-
-    describe('#parser', () => {
-      it('extracts protopipe opt from params', () => {
-        const handler: IHandler = (input: IInput): IOutput => input
-        const graph: IGraph = new Graph({
-          vertexes: [],
-          arrows: [],
-        })
-        const traverser: ITraverser = (input: ITraverserInput): ITraverserOutput => input
-        const executor: IExecutor = (context: IExecutorContext): IExecutorOutput => context
-        const params = {
-          graph,
-          handler,
-          traverser,
-          executor,
-        }
-        const opts = Protopipe.parser(params)
-
-        expect(opts).toEqual(params)
-      })
-
-      it('injects default values otherwise', () => {
-        const opts = Protopipe.parser({})
-
-        expect(opts).toEqual({
-          graph: Protopipe.graph,
-          handler: Protopipe.handler,
-          traverser: Protopipe.traverser,
-          executor: Protopipe.executor,
-        })
-      })
-
-      it('supports parser impl override', () => {
-        const opts = Protopipe.parser({
-          parser() {
-            return {
-              graph: Protopipe.graph,
-              handler: Protopipe.handler,
-              traverser: Protopipe.traverser,
-              executor: Protopipe.executor,
-              foo: 'bar',
-            }
-          },
-        })
-
-        expect(opts).toEqual({
-          graph: Protopipe.graph,
-          handler: Protopipe.handler,
-          traverser: Protopipe.traverser,
-          executor: Protopipe.executor,
-          foo: 'bar',
-        })
       })
     })
   })
