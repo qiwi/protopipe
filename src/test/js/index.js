0ddf235582dabedb60dba544126e4fd10021729f
Anton Golub <mailbox@antongolub.ru>
2019-05-15 17:27:56 +0300

    fix(Injector): correct upsert flow

--- src/test/js/index.js
+++ src/test/js/index.js
@@ -1,8 +1,7 @@
-const {Graph, Protopipe} = require('../../../lib/es5')
+const {Graph} = require('../../../lib/es5')
 
 describe('index', () => {
   it('exposes lib inners', () => {
     expect(Graph).toEqual(expect.any(Function))
-    expect(Protopipe).toEqual(expect.any(Function))
   })
 })
