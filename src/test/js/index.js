594df302f20eb2127dd12759a589715383b4cb29
Anton Golub <mailbox@antongolub.ru>
2019-07-10 22:13:28 +0300

    perf: uglify published files

--- src/test/js/index.js
+++ src/test/js/index.js
@@ -4,4 +4,27 @@ describe('index', () => {
   it('exposes lib inners', () => {
     expect(Graph).toEqual(expect.any(Function))
   })
+
+  it('works after uglify', () => {
+    const vertexes = []
+    const edges = []
+    const features = {}
+    const incidentor = {
+      type: 'EDGE_LIST_INCDR',
+      value: {},
+    }
+    const graphParams = {
+      vertexes,
+      edges,
+      incidentor,
+      features,
+    }
+    const graph = new Graph(graphParams)
+
+    expect(graph).toBeInstanceOf(Graph)
+    expect(graph.vertexes).toBe(vertexes)
+    expect(graph.edges).toBe(edges)
+    expect(graph.incidentor).toBe(incidentor)
+    expect(graph.features).toBe(features)
+  })
 })
